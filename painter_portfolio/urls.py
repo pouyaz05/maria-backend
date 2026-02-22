7dda1d3a8b02f06479f4c60f2d8e4a53519a8f41
pouyaz05 <pouyazamani00@gmail.com>
2026-02-22 12:57:08 +0330

    Ready for Liara deploy

--- painter_portfolio/urls.py
+++ painter_portfolio/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("gallery.urls")),
+
+    # API ها
+    path("api/", include("gallery.urls")),
 ]
 
+# React SPA Catch-All
+urlpatterns += [
+    re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
+]
+
+# فقط در حالت توسعه
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
