4b22a629489df6f988e2db9e89cd3f3e5c3b1bc8
pouyaz05 <pouyazamani00@gmail.com>
2026-02-24 01:40:53 +0330

    add viewS

--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,2 @@
 def painting_list_api(request):
-    paintings = Painting.objects.all().order_by("-id")
-
-    data = [{
-        "id": p.id,
-        "title": p.title,
-        "artist_name": p.artist_name,
-        "description": p.description,
-        "year": p.year_created,
-        "image": p.image.url if p.image else None,
-    } for p in paintings]
-
-    return JsonResponse(data, safe=False)
+    return JsonResponse({"test": "ok"})
