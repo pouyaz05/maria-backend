f7edf15560dac8ccee752a189bfcae0824b81942
pouyaz05 <pouyazamani00@gmail.com>
2026-02-24 01:23:45 +0330

    add urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import OrderCreateView, OrderListView, painting_list_api
 
 urlpatterns = [
-    path("api/orders/create/", OrderCreateView.as_view(), name="create_order"),
-    path("api/orders/list/", OrderListView.as_view(), name="order_list"),
-    path("api/paintings/", painting_list_api, name="painting_list_api"),
+    path("orders/create/", OrderCreateView.as_view(), name="create_order"),
+    path("orders/list/", OrderListView.as_view(), name="order_list"),
+    path("paintings/", painting_list_api, name="painting_list_api"),
 ]
 
--- painter_portfolio/urls.py
+++ painter_portfolio/urls.py
@@ -7,14 +7,19 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # API ها
+    # API
     path("api/", include("gallery.urls")),
 ]
 
 # React SPA Catch-All
+# هر چیزی که api و static و media نباشد → index.html
 urlpatterns += [
-    re_path(r"^(?!api/|static/).*", TemplateView.as_view(template_name="index.html")),
+    re_path(
+        r"^(?!api/|static/|media/).*",
+        TemplateView.as_view(template_name="index.html"),
+    ),
 ]
-# فقط در حالت توسعه
+
+# فقط در حالت توسعه (لوکال)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
