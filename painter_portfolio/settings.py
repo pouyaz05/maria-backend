909e4e6cd82868ba04ede903e0c5300c3bc48761
pouyaz05 <pouyazamani00@gmail.com>
2026-02-24 18:31:32 +0330

    add CSS

--- painter_portfolio/settings.py
+++ painter_portfolio/settings.py
@@ -83,10 +83,11 @@ TEMPLATES = [
 # =========================
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles") 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static") 
+]
 # =========================
 # MEDIA
 # =========================
